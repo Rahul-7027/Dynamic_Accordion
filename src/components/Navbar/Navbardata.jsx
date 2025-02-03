4b85833c44feb28560174c60415eb6b80dd20141
Rahul Bhati <rahulbhati0202@gmail.com>
2025-02-03 20:22:37 +0530

    hamburger add on

--- src/components/Navbar/Navbardata.jsx
+++ src/components/Navbar/Navbardata.jsx
@@ -1,17 +1,30 @@
-import React from 'react'
-import "./nav.css"
+import React, { useState } from "react";
+import "./nav.css";
+import { GiHamburgerMenu } from "react-icons/gi";
 
-const Navbardata = () => {
-    return (
-        <div>
-            <ul>
-                <li>CodeWithRahulBhati</li>
-                <li>Home</li>
-                <li>About</li>
-                <li>Contact US</li>
-            </ul>
-        </div>
-    )
-}
+const NavbarData = () => {
+  const [active, setActive] = useState(false);
 
-export default Navbardata
+  const handleActive = () => {
+    setActive(!active);
+  };
+
+  return (
+    <nav className="menu-container">
+      {/* Hamburger Menu */}
+      <div className="ham-menu" onClick={handleActive}>
+        <GiHamburgerMenu />
+      </div>
+
+      {/* Navigation Menu */}
+      <ul className={`menu ${active ? "menu-mobile" : "menu-web"}`}>
+        <li className="menu-item">CodeWithRahulBhati</li>
+        <li className="menu-item">Home</li>
+        <li className="menu-item">About</li>
+        <li className="menu-item">Contact Us</li>
+      </ul>
+    </nav>
+  );
+};
+
+export default NavbarData;
